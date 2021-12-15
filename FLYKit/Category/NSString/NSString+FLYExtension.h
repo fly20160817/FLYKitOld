3e464ca472ce3558cc3d484053a7ff68fbb08a67
“fly” <809176334@qq.com>
2021-12-15 11:36:57 +0800

    NSString+FLYExtension分类新增json字符串转数组、字典方法

--- FLYKit/Category/NSString/NSString+FLYExtension.h
+++ FLYKit/Category/NSString/NSString+FLYExtension.h
@@ -14,8 +14,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// 数组、字典转json字符串
 /// @param object 数组或字典
+/// @return string json字符串
 + (NSString *)objectToJson:(id)object;
 
+
+/// json字符串转数组、字典
+/// @param jsonString json字符串
+/// @return id 数组、字典
++ (id)jsonToObject:(NSString *)jsonString;
 
 @end
 
--- FLYKit/Category/NSString/NSString+FLYExtension.m
+++ FLYKit/Category/NSString/NSString+FLYExtension.m
@@ -13,11 +13,23 @@
 /// @param object 数组或字典
 + (NSString *)objectToJson:(id)object
 {
-    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:object options:0 error:nil];
+    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:object options:NSJSONWritingPrettyPrinted error:nil];
     
     NSString * jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     
     return jsonString;
+}
+
+/// json字符串转数组、字典
+/// @param jsonString json字符串
+/// @return id 数组、字典
++ (id)jsonToObject:(NSString *)jsonString
+{
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:nil];
+    
+    return dic;
 }
 
 @end
